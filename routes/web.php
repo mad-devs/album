00b4c806c3d963c607d2437e317b68b1d5172da4
AdoDeveloper <105835711+AdoDeveloper@users.noreply.github.com>
2024-12-31 01:39:28 -0600

    Ajustando ruta de login

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
